4402773437d99d2453d3d338d6c6706160f19912
Sumit <sumit.58r@gmail.com>
2022-05-23 22:22:33 +0530

    final commit

--- 1.Variables and datatypes/input_function.py
+++ 1.Variables and datatypes/input_function.py
@@ -1,4 +1,4 @@
 a = input("Enter you Name ")
-#a =int(a)  To convert from string to an Int (if possible)
-#print(type(a))
+a  =int(a)  #To convert from string to an Int (if possible)
+print(type(a))
 print((a))  
